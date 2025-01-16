b7569e324420204ba91926c04011222b9a6b440d
hughbuntine <hbuntine@student.unimelb.edu.au>
2025-01-16 14:17:40 +0200

    refactored

--- index.js
+++ index.js
@@ -1,41 +1,17 @@
 const express = require('express')
 const app = express()
 app.use(express.json());
+
 const cors = require('cors')
-const mongoose = require('mongoose')
 const connectDB = require('./mongo.js');
 require('dotenv').config();
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 
 app.use(cors())
-app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+const blogsRouter = require('./controllers/blogs.js')
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 // Connect to DB then start server
 const PORT = 3003
@@ -45,3 +21,4 @@ connectDB().then(() => {
   })
 }
 )
+
